7ffa9c96dad25566bd04f08c57e8521ef06096bc
Varagan77 <johanneswillemkotze@gmail.com>
2025-04-06 08:15:15 +0200

    boilerplate
    basic boilerplate for JDS. The start of everything.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './assets/App.css'
+import './assets/index.css';
+function App() {
+
+  return (
+    <>
+      <h1>JDS APP</h1>
+      <button>App</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/App.css'
+import './assets/index.css';
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>&copy; JDS FOOTER</p>
+      </footer>
+  );
+};
+
+
+export default Footer;
